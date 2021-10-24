61ada0ef5f94e95059bccff968c5be2238b1b8e2
Tom Hilton <tomhilton93@gmail.com>
2021-10-24 17:27:18 +0100

    extend sans font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ['Inter'],
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
     },
     extend: {},
   },
